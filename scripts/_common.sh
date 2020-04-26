359a74f2e8b0afb2c737129735b5e6d6484fa701
philippe lhardy <pl@artisanlogiciel.net>
2020-04-26 23:27:08 +0200

    set APP_PATH for api entry point usage
    - client needs to know its APP_PATH to be delgated to correct app
    - and forget about API_ENTRY_POINT that was replaced by APP_PATH

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@
 #trivabble_git_tag=prod-v20200425.3
 
 trivabble_git_url=https://gitlab.com/artlog/trivabble.git
-trivabble_git_tag=test-v20200425
+trivabble_git_tag=test-v202004262313
 
 # dependencies used by the app
 pkg_dependencies="nodejs"
