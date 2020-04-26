f8e7aa69ff7f8531a50b88bb949b4228e83e74a7
philippe lhardy <pl@artisanlogiciel.net>
2020-04-26 17:57:04 +0200

    Use a forked trivabble git project to add required feature on yunohost
    - https://gitlab.com/artlog/trivabble.git tag test-v20200425
    - purpose is to move url to server api within yunohsot app subdirectory range

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,8 +5,11 @@
 #=================================================
 
 # git url to retrieve original source
-trivabble_git_url=https://gitlab.com/raphj/trivabble.git
-trivabble_git_tag=prod-v20200425.3
+#trivabble_git_url=https://gitlab.com/raphj/trivabble.git
+#trivabble_git_tag=prod-v20200425.3
+
+trivabble_git_url=https://gitlab.com/artlog/trivabble.git
+trivabble_git_tag=test-v20200425
 
 # dependencies used by the app
 pkg_dependencies="nodejs"
