632e82cf3f75909e68adce45d0aedb0b6a9b4351
philippe lhardy <pl@artisanlogiciel.net>
2020-05-20 16:56:33 +0200

    use conf/app.src on tar.gz provided by gitlab ( any tag will have its tar.gz source autocreated by github )
    SOURCE_URL should be full url
    SOURCE_FILENAME is not needed
    
    align version  name to source tag

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
+# should somehow relate to app.src
 # git url to retrieve original source
 trivabble_git_url=https://gitlab.com/raphj/trivabble.git
 trivabble_git_tag=prod-v20200503
